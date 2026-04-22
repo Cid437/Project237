61962d8cc08d1bcf70474710350df662050336a5
Cid437 <donntorres26@gmail.com>
2026-04-22 19:02:33 +0800

    customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'customers';
+    protected $primaryKey = 'customer_id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'user_id',
+        'fname',
+        'lname',
+        'phone',
+        'addressline',
+        'zipcode',
+        'image_path',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'customer_id', 'customer_id');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'customer_id', 'customer_id');
+    }
 }
