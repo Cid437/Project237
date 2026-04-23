b4e83008107afdac529362b882268831f1785767
Cid437 <donntorres26@gmail.com>
2026-04-23 10:24:45 +0800

    Install api

---
+++ resources/views/remember.blade.php
@@ -0,0 +1,34 @@
+@section('body')
+
+{{--
+
+    Create Laravel file
+        composer create-project laravel/laravel projName
+    Create a controller
+        php artisan make:controller controllerName
+        php artisan make:controller controllerName --resource
+        php artisan make:controller controllerName --api    
+    Create a model
+        php artisan make:model modelName
+    Create a view
+        php artisan make:view viewName
+    Create a route
+        php artisan make:route routeName
+    create a migration
+        php artisan make:migration create_tablename_table
+    run migration
+        php artisan migrate
+    run migration with seeding
+        php artisan migrate --seed
+    run seeding
+        php artisan db:seed
+    run migration with fresh
+        php artisan migrate:fresh
+    run migration with fresh and seeding
+        php artisan migrate:fresh --seed
+    run server
+        php artisan serve    
+
+--}}
+
+@endsection
